ea971a77a7286b0a95bcdebef7452c0c5c93d620
00-status <dedypips@gmail.com>
2024-06-24 11:15:30 -0600

    Added stylings to Roster Items

--- src/components/RosterItem.tsx
+++ src/components/RosterItem.tsx
@@ -10,20 +10,22 @@ export const RosterItem = (props: Props) => {
     const { pokemon, onClick } = props;
     return <div className="roster-item">
         <img className="roster-item__image" src={pokemon.sprite} alt={pokemon.name} />
-        <div className="roster-item__label">
+        <div className="roster-item__contents">
             <div>
-                {pokemon.name}
+                <div className="roster-item__label">
+                    <div>
+                        {pokemon.name}
+                    </div>
+                    {pokemon.types.map(typeName => <div className='roster-item__pill'>{typeName}</div>)}
+                </div>
+                <div>
+                    <b>Height:</b> {pokemon.height / 10}m.
+                </div>
+                <div>
+                    <b>Weight:</b> {pokemon.weight / 10}kg.
+                </div>
             </div>
-            <div>
-                {pokemon.types.map(typeName => typeName)}
-            </div>
-            <div>
-                {pokemon.height / 10}m.
-            </div>
-            <div>
-                {pokemon.weight / 10}kg.
-            </div>
+            <button className='roster-item__button' onClick={() => onClick(pokemon.name)}>X</button>
         </div>
-        <button onClick={() => onClick(pokemon.name)}>Remove</button>
     </div>;
 };
